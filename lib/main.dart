4a31ee14a2744be963b2b62863d089ce5f499b80
Bilal <bilalfahiiim@gmail.com>
2022-02-12 23:46:53 +0500

    --

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'package:store_app/screens/splash_screen.dart';
+
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+//theme data
+
+      theme: ThemeData(
+        primaryColor: Colors.redAccent,
+        primaryColorLight: Colors.white,
+        primaryColorDark: Colors.black,
+        primarySwatch: Colors.red,
+        scaffoldBackgroundColor: Colors.white,
+        dividerColor: Color(0xFF202020),
+        hintColor: Colors.white,
+
+        //Button theme
+
+        buttonTheme: ButtonThemeData(
+            buttonColor: Theme.of(context).primaryColor,
+            splashColor: Colors.red),
+
+        //Text theme
+
+        textTheme: TextTheme(
+            button: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+                color: Colors.white70),
+            bodyText1: TextStyle(fontSize: 20, color: Colors.white70),
+            subtitle2: TextStyle(fontSize: 20, color: Colors.white38)),
+      ),
+
+      home: Splash_screen(),
+    );
+  }
+}
+
+const blue = Color(0xFF3b5998);
+const grey = Color(0xff383838);
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:store_app/screens/login_screen.dart';
+
+class Splash_screen extends StatefulWidget {
+  const Splash_screen({Key? key}) : super(key: key);
+
+  @override
+  _Splash_screenState createState() => _Splash_screenState();
+}
+
+class _Splash_screenState extends State<Splash_screen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        (() => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => Login_screen()))));
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Image.asset(
+        "assets/logo.png",
+        height: MediaQuery.of(context).size.height * 0.4,
+        width: MediaQuery.of(context).size.width * 0.4,
+      )),
+    );
+  }
+}
